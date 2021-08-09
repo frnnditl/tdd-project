997885f3d26be7e34b37d96ede726077b5fe6ea3
frnnditl <linkaccounts@live.com>
2021-08-09 19:59:31 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 4

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
